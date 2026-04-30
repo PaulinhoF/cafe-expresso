05e1c15e00498f404f5124dfa4ea3f222bec895a
PaulinhoF <278194983+PaulinhoF@users.noreply.github.com>
2026-04-30 12:50:18 -0300

    Testando primeiro teste (e falhando)

---
+++ ItemPedidoTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ItemPedidoTest {
+
+    @Test
+    public void testaCalculoDoSubtotal() {
+        Produto produto = new Produto("cafe", 5.5);
+        ItemPedido pedido = new ItemPedido(produto, 3);
+        double subtotal = produto.getPrecoUnitario() * pedido.getQuantidade();
+        assertEquals(16.5, subtotal, 0.01);
+    }
+
+}
