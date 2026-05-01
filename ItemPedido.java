a50c0faa00e8ccda1cfc4a84f91b6b3b5723d730
PaulinhoF <278194983+PaulinhoF@users.noreply.github.com>
2026-05-01 20:48:04 -0300

    Primeiro teste passando (hopefully)

---
+++ ItemPedido.java
@@ -0,0 +1,26 @@
+public class ItemPedido {
+
+    private Produto produto;
+    private int quantidade;
+
+    public ItemPedido(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double calcularSubtotal(){
+        produto = new Produto("Café", 5.5);
+        ItemPedido pedido = new ItemPedido(produto, 3);
+        double subtotal = produto.getPrecoUnitario() * pedido.getQuantidade();
+        return subtotal;
+    }
+
+}
