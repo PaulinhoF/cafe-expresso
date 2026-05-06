68703bdd485049dfa3cd78d2993568781eac3843
PaulinhoF <278194983+PaulinhoF@users.noreply.github.com>
2026-05-06 19:34:57 -0300

    Testes para preços e quantidades negativas em calcularSubtotal()

--- src/main/java/ItemPedido.java
+++ src/main/java/ItemPedido.java
@@ -17,7 +17,12 @@ public class ItemPedido {
     }
 
     public double calcularSubtotal() {
+        
         double subtotal = produto.getPrecoUnitario() * quantidade;
+        if(produto.getPrecoUnitario()<0)
+            throw new Error("Preço inválido!");
+        if(quantidade<1)
+            throw new Error("Quantidade inválida!");
         return subtotal;
     }
 
--- src/test/java/ItemPedidoTest.java
+++ src/test/java/ItemPedidoTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ItemPedidoTest {
 
@@ -10,5 +11,27 @@ public class ItemPedidoTest {
         cafe.calcularSubtotal();
         assertEquals(22, cafe.calcularSubtotal(), 0.01);
     }
+
+    @Test
+    public void testaCalculoDoSubtotalDeveLancarErroQuandoPrecoNegativo() {
+        Produto produto = new Produto("pao", -0.01);
+        ItemPedido cafe = new ItemPedido(produto, 2);
+        Error erroCapturado = assertThrows(Error.class, () -> {
+            cafe.calcularSubtotal();
+        });
+        assertEquals("Preço inválido!", erroCapturado.getMessage());
+    }
+    
+    @Test
+    public void testaCalculoDoSubtotalDeveLancarErroQuandoQuantidadeAbaixoDeUm() {
+        Produto produto = new Produto("chá", 3.50);
+        ItemPedido cha = new ItemPedido(produto, 0);
+        Error erroCapturado = assertThrows(Error.class, () -> {
+            cha.calcularSubtotal();
+        });
+        assertEquals("Quantidade inválida!", erroCapturado.getMessage());
+    }
+
+
 
 }
